7dce2a3e32dbc515026fe8759ac0d2831eca85aa
David Olivari <david.olivari@gmail.com>
2024-01-30 05:19:58 +0100

    feat: removing lazyimage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script>
   import { page } from '$app/stores'
   import { goto } from '$app/navigation'
-  import { useLazyImage as lazyImage } from 'svelte-lazy-image'
   import '$lib/scss/zooms.scss'
   import { onMount } from 'svelte'
 
